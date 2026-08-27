79871c2a79012ec43f065039fb174112e927e4a4
Hemasundar2006 <marothihemasundar03@gmail.com>
2026-08-28 02:14:31 +0530

    feat: restrict CORS origins to specific allowlist and support environment variables

--- server.js
+++ server.js
@@ -25,7 +25,14 @@ const { swaggerUi, specs } = require('./config/swagger');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://agrinex.vercel.app',
+    process.env.FRONTEND_URL
+  ].filter(Boolean),
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
